ed6df216654b3f5934f60addbc77d3cdffedefd6
SUBHAJIT GHOSH <subhajitghosh@SUBHAJITs-Mac-mini.local>
2025-11-18 12:51:49 +0530

    update on 18-11-25

--- src/screens/NoAuthScreen/Customer/ChatScreen.js
+++ src/screens/NoAuthScreen/Customer/ChatScreen.js
@@ -1907,7 +1907,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 20,
+    //padding: 20,
   },
   buttonImage: {
     width: 100,
